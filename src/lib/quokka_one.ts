278277bcdca82b0f69f998e7ce37be1d1adff818
Chris Roberts <chris@rowberts.com>
2023-02-26 21:08:47 -0600

    New install

---
+++ src/lib/quokka_one.ts
@@ -0,0 +1,4 @@
+import { logSomething } from '$lib/quokka_two.ts'; // Fails
+// import { logSomething } from './quokka_two'; // Works
+
+logSomething;
---
+++ src/lib/quokka_two.ts
@@ -0,0 +1,3 @@
+export function logSomething() {
+	console.log('This is where the utility functions go');
+}
